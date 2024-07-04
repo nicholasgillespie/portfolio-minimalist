becf0bcbe7a51da86e99bc31ee4192cf97ca5d09
Nicholas Gillespie <contact@nicholasgillespie.com>
2024-07-04 16:23:19 +0200

    chore: Update astro.config.mjs to include SITE_BASE and remove commented out trailingSlash configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 import { defineConfig } from "astro/config";
 import purgecss from "astro-purgecss";
+import { SITE_BASE } from "./src/consts";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [purgecss()],
   site: "https://nicholasgillespie.github.io",
+  base: SITE_BASE,
+  trailingSlash: "never",
 });
--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
+export const SITE_BASE = "/portfolio-minimalist";
 export const SITE_TITLE = "Portfolio Minimalist";
 export const SITE_NAVIGATION = [
   { label: "Home", url: "/" },
