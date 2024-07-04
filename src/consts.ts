a0ab8ff636c4ca33e0929d8aff03f07e7cbb75e4
Nicholas Gillespie <contact@nicholasgillespie.com>
2024-07-04 16:28:00 +0200

    chore: Update astro.config.mjs to use CONFIG_BASE constant for base configuration

--- src/consts.ts
+++ src/consts.ts
@@ -1,7 +1,8 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_BASE = "/portfolio-minimalist";
+export const CONFIG_BASE = "/portfolio-minimalist";
+
 export const SITE_TITLE = "Portfolio Minimalist";
 export const SITE_NAVIGATION = [
   { label: "Home", url: "/" },
