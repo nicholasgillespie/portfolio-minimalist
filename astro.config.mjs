90ce8410119dac6a2c374de39d6e8c65cec39263
Nicholas Gillespie <contact@nicholasgillespie.com>
2024-07-04 16:25:57 +0200

    chore: Update astro.config.mjs to use SITE_BASE constant for base configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig } from "astro/config";
 import purgecss from "astro-purgecss";
-import { SITE, BASE } from "./src/consts";
+import { SITE_BASE } from "./src/consts";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [purgecss()],
-  site: SITE,
-  base: BASE,
+  site: "https://nicholasgillespie.github.io",
+  base: SITE_BASE,
   trailingSlash: "never",
 });
--- src/consts.ts
+++ src/consts.ts
@@ -1,9 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE = "https://nicholasgillespie.github.io";
-export const BASE = "/portfolio-minimalist";
-
+export const SITE_BASE = "/portfolio-minimalist";
 export const SITE_TITLE = "Portfolio Minimalist";
 export const SITE_NAVIGATION = [
   { label: "Home", url: "/" },
