6d3691c5b11fc3040d8877014b4a6432a0aed21e
Nicholas Gillespie <contact@nicholasgillespie.com>
2024-07-05 13:24:54 +0200

    test purgecss.

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import { CONFIG_SITE, CONFIG_BASE } from "./src/consts";
 
 // https://astro.build/config
 export default defineConfig({
-  // integrations: [purgecss()],
+  integrations: [purgecss()],
   site: CONFIG_SITE,
   base: CONFIG_BASE,
   // trailingSlash: "never",
