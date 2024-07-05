1043ddb2fcd31726527a1853da2546b410ed4fd1
Nicholas Gillespie <contact@nicholasgillespie.com>
2024-07-05 13:21:52 +0200

    Test prod without prugecss.

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,8 @@ import { CONFIG_SITE, CONFIG_BASE } from "./src/consts";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [purgecss()],
-  site: CONFIG_SITE,
-  base: CONFIG_BASE,
+  // integrations: [purgecss()],
+  // site: CONFIG_SITE,
+  // base: CONFIG_BASE,
   // trailingSlash: "never",
 });
